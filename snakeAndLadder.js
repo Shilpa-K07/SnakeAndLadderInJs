9574cac678727a88be0c08aa281eb5896e5d9cbb
shilpa <shilpa07udupi@gmail.com>
2020-11-17 18:37:00 +0530

    feat: check option and then change position

--- snakeAndLadder.js
+++ snakeAndLadder.js
@@ -1,6 +1,7 @@
 let position = 0;
 
 const randomValueBetweenOneToSix = () =>  Math.floor(Math.random()*10)%6+1;
+const randomValueBetweenOneToThree = () => Math.floor(Math.random()*10)%3;
 
 /**
  * @description genereating random number between one to six 
@@ -10,4 +11,36 @@ var rollDieToGetNumber = () => {
         var randomNumber = randomValueBetweenOneToSix();
 		return randomNumber;
 }
-console.log(rollDieToGetNumber());
+
+/**
+ * @description checking option using random value
+ * @method randomValueBetweenOneToThree generates random number
+ * @var option is set based on random number
+ */
+
+ var checkOption = (dieNumber) => {
+        var option = null;
+		var randomNumber = randomValueBetweenOneToThree();
+		switch(randomNumber)
+		{
+			case 0:
+			option = "No Play";
+			position = position;
+			break;
+
+			case 1:
+			option = "Ladder";
+			position += dieNumber;
+			break;
+			
+			default:
+			option = "Snake";
+			position -= dieNumber;
+		}
+		console.log(" Option is : " +option);
+		console.log(" Current Position is : " +position+"\n");
+
+ }
+var dieNumber = rollDieToGetNumber();
+checkOption(dieNumber);
+
