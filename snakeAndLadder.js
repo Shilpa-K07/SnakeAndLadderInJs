452b75d514136df472f261245d0882dc0bc45d1b
shilpa <shilpa07udupi@gmail.com>
2020-11-17 18:02:34 +0530

    feat: Roll die to get number

--- snakeAndLadder.js
+++ snakeAndLadder.js
@@ -1,1 +1,13 @@
 let position = 0;
+
+const randomValueBetweenOneToSix = () =>  Math.floor(Math.random()*10)%6+1;
+
+/**
+ * @description genereating random number between one to six 
+ * @method randomValueBetweenOneToSix generates random number
+ */ 
+var rollDieToGetNumber = () => {
+        var randomNumber = randomValueBetweenOneToSix();
+		return randomNumber;
+}
+console.log(rollDieToGetNumber());
